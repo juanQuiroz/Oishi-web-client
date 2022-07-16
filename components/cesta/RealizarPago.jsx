780785709253dd0ed3f9235477e8ba12861d12f0
Hector <hectorbenavente@gmail.com>
2022-07-16 13:15:02 -0500

    testing next api izipaytest porject

--- components/cesta/RealizarPago.jsx
+++ components/cesta/RealizarPago.jsx
@@ -70,7 +70,7 @@ const RealizarPago = ({ dataPedido, entregaDelivery }) => {
       const KRGlue = glue.default;
 
       axios
-        .post("https://oishi-web-client.vercel.app/api/createPayment", {
+        .post("https://test-izipay-delete.vercel.app/api/createPayment", {
           amount:
             dataPedido.total_price * 100 + dataPedido.delivery_price * 100,
           currency: "PEN",
