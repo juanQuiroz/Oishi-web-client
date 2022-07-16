f87d5f5265bdefab5fabe31fa08707c8859ab58d
Hector <hectorbenavente@gmail.com>
2022-07-16 01:39:07 -0500

    consolelog test

--- pages/api/createPayment.js
+++ pages/api/createPayment.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const body = req.body;
 
+    console.log("LLEGO HASTA API/CREATEPAYMENT");
+
     try {
       const response = await axios.post(
         "https://api.micuentaweb.pe/api-payment/V4/Charge/CreatePayment",
@@ -20,7 +22,7 @@ export default async function handler(req, res) {
               "Basic NTcxODA2ODU6dGVzdHBhc3N3b3JkX0R1aXRURTBJaHhZMjhTemtjUXkyWVBodXdjVXUzNzdVeUdDWXl0YzE5YlROZA==",
             // "Basic NTcxODA2ODU6cHJvZHBhc3N3b3JkX082ZlgyaHM0bTRXdFRzUE5SVDdZblo2N3d4OUZmc0d5bHl4aEt4QU9mZEhvbQ==",
           },
-        },
+        }
       );
 
       console.log("ApiNextJS - TRY -> ", response);
@@ -32,25 +34,3 @@ export default async function handler(req, res) {
     }
   }
 }
-
-// const createPayment = async body => {
-//   try {
-//     const res = await axios.post(
-//       "https://api.micuentaweb.pe/api-payment/V4/Charge/CreatePayment",
-//       body,
-//       {
-//         headers: {
-//           "Access-Control-Allow-Origin": "*",
-//           "Content-Type": "application/json",
-//           Authorization:
-//             "Basic NTcxODA2ODU6dGVzdHBhc3N3b3JkX0R1aXRURTBJaHhZMjhTemtjUXkyWVBodXdjVXUzNzdVeUdDWXl0YzE5YlROZA==",
-//         },
-//       },
-//     );
-
-//     console.log(crearCompra);
-//     res.status(201).json(res);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
